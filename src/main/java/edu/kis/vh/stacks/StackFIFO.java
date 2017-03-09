4e4a429872a62c3cf7242f81eddb57aeb34bad91
Maciej Kacprzak <194971@edu.p.lodz.pl>
2017-03-09 14:57:37 +0100

    Refactor file StackFIFO.java

--- src/main/java/edu/kis/vh/stacks/StackFIFO.java
+++ src/main/java/edu/kis/vh/stacks/StackFIFO.java
@@ -6,15 +6,15 @@ public class StackFIFO extends stack {
 	
 	@Override
 	public int pop() {
-		while (!isEmpty())
-		
-		temp.push(super.pop());
+		while (!isEmpty()) {
+			temp.push(super.pop());
+		}
 		
 		int ret = temp.pop();
 		
-		while (!temp.isEmpty())
-	
-		push(temp.pop());
+		while (!temp.isEmpty()) {
+			push(temp.pop());
+		}
 		
 		return ret;
 	}
