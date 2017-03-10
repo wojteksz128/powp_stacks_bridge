2cc5187bc9df78908e9fe9a459774b7cdd72eb9d
Szczepaniak.Wojciech <195067@edu.p.lodz.pl>
2017-03-10 14:56:42 +0100

    Przeniesienie klasy node wewnątrz klasy StackList

--- src/main/java/edu/kis/vh/stacks/list/StackList.java
+++ src/main/java/edu/kis/vh/stacks/list/StackList.java
@@ -37,5 +37,38 @@ public class StackList {
 		last = last.getPrev();
 		return ret;
 	}
+	
+	class Node {
+
+		private final int value;		// Zmiana odwoływania się do pola za pomocą getera
+		private Node prev;		// Zmiana odwoływania się do pola za pomocą getera
+		private Node next;		// Zmiana odwoływania się do pola za pomocą getera
+
+		public Node(int i) {
+			value = i;
+		}
+
+		int getValue() {
+			return value;
+		}
+
+		Node getPrev() {
+			return prev;
+		}
+
+		void setPrev(Node prev) {
+			this.prev = prev;
+		}
+
+		Node getNext() {
+			return next;
+		}
+
+		void setNext(Node next) {
+			this.next = next;
+		}
+
+	}
+
 
 }
