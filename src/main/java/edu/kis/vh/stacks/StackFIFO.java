e14eefbe5836244179d547f6e5148aea02045c91
Szczepaniak.Wojciech <195067@edu.p.lodz.pl>
2017-03-12 13:24:48 +0100

    Add javadoc information into the StackFIFO class

--- src/main/java/edu/kis/vh/stacks/StackFIFO.java
+++ src/main/java/edu/kis/vh/stacks/StackFIFO.java
@@ -1,9 +1,23 @@
 package edu.kis.vh.stacks;
 
+
+/**
+ * Implementation of <a href="https://en.wikipedia.org/wiki/Queue_(abstract_data_type)">queue</a> data structure. This structure implement two methods:
+ * <br>   - {@link StackFIFO#push} - add element into the queue,
+ * <br>   - {@link StackFIFO#pop} - get first element from the queue and remove it.
+ * 
+ * @author Wojciech Szczepaniak
+ *
+ */
 public class StackFIFO extends Stack {
 
 	private final Stack stack = new Stack();
 
+	/**
+	 * Return first inserted element and remove it. If queue is empty, than return {@link Stack#STACK_EMPTY}.
+	 * 
+	 * @return last inserted element
+	 */
 	@Override
 	public int pop() {
 		// TODO: This implementation may produce problems when after pop() you push() new element. Maybe list is better option?
