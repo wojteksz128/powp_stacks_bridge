65089eac14e9b1d6b7258df2c3bab01040c1055b
Szczepaniak.Wojciech <195067@edu.p.lodz.pl>
2017-03-12 13:49:49 +0100

    Add javadoc to StacksFactory and default this implementation.

--- src/main/java/edu/kis/vh/stacks/factory/DefaultStacksFactory.java
+++ src/main/java/edu/kis/vh/stacks/factory/DefaultStacksFactory.java
@@ -5,24 +5,42 @@ import edu.kis.vh.stacks.Stack;
 import edu.kis.vh.stacks.StackFIFO;
 import edu.kis.vh.stacks.StackHanoi;
 
+/**
+ * Default implementation of {@link StacksFactory}.
+ * 
+ * @author Wojciech Szczepaniak
+ *
+ */
 public class DefaultStacksFactory implements StacksFactory {
 
 	// TODO: What are the differences between methods getStandardStack() and getFalseStack()?
+	/* (non-Javadoc)
+	 * @see edu.kis.vh.stacks.factory.StacksFactory#getStandardStack()
+	 */
 	@Override
 	public Stack getStandardStack() {
 		return new Stack();
 	}
 
+	/* (non-Javadoc)
+	 * @see edu.kis.vh.stacks.factory.StacksFactory#getFalseStack()
+	 */
 	@Override
 	public Stack getFalseStack() {
 		return new Stack();
 	}
 
+	/* (non-Javadoc)
+	 * @see edu.kis.vh.stacks.factory.StacksFactory#getFIFOStack()
+	 */
 	@Override
 	public StackFIFO getFIFOStack() {
 		return new StackFIFO();
 	}
 
+	/* (non-Javadoc)
+	 * @see edu.kis.vh.stacks.factory.StacksFactory#getHanoiStack()
+	 */
 	@Override
 	public StackHanoi getHanoiStack() {
 		return new StackHanoi();
--- src/main/java/edu/kis/vh/stacks/factory/StacksFactory.java
+++ src/main/java/edu/kis/vh/stacks/factory/StacksFactory.java
@@ -4,14 +4,40 @@ import edu.kis.vh.stacks.Stack;
 import edu.kis.vh.stacks.StackFIFO;
 import edu.kis.vh.stacks.StackHanoi;
 
+/**
+ * Interface of methods to creating factory of stacks.
+ * 
+ * @author Wojciech Szczepaniak
+ *
+ */
 public interface StacksFactory {
 
+	/**
+	 * Returned new empty {@link Stack}.
+	 * 
+	 * @return new empty stack
+	 */
 	public Stack getStandardStack();
  
+	/**
+	 * Returned new empty {@link Stack}.
+	 * 
+	 * @return new empty stack
+	 */
 	public Stack getFalseStack();
 
+	/**
+	 * Returned new empty {@link StackFIFO}.
+	 * 
+	 * @return new empty queue
+	 */
 	public StackFIFO getFIFOStack();
 
+	/**
+	 * Returned new empty {@link StackHanoi}.
+	 *  
+	 * @return new empty StackHanoi
+	 */
 	public StackHanoi getHanoiStack();
 
 }
