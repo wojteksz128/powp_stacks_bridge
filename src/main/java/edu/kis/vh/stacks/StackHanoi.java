4a307566171fc8bb30ec93f6a39166560fe334d4
Szczepaniak.Wojciech <195067@edu.p.lodz.pl>
2017-03-12 13:42:03 +0100

    Add javadoc information into the StackHanoi class.

--- src/main/java/edu/kis/vh/stacks/StackHanoi.java
+++ src/main/java/edu/kis/vh/stacks/StackHanoi.java
@@ -1,13 +1,29 @@
 package edu.kis.vh.stacks;
 
+/**
+ * Implementation of stack on the <a href="https://en.wikipedia.org/wiki/Tower_of_Hanoi">Tower of Hanoi</a> problem. You can push into the stack only element not greater than last pushed element.
+ * 
+ * @author Wojciech Szczepaniak
+ *
+ */
 public class StackHanoi extends Stack {
 
 	private int totalRejected = 0;
 
+	/**
+	 * Return number of wrong inserted into the stack. 
+	 * 
+	 * @return number of wrong inserted
+	 */
 	public int reportRejected() {
 		return totalRejected;
 	}
 
+	/**
+	 * Insert value into the stack. If added element is greater than last inserted element, this element is not inserted, and number of wrong inserts is increasing by 1.
+	 * 
+	 * @param in added value
+	 */
 	@Override
 	public void push(int in) {
 		if (!isEmpty() && in > top()) {
