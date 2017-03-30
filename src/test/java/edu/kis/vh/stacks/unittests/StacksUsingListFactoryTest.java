946152aaaf5e83092f2e6557587f1097cfca8529
Szczepaniak.Wojciech <195067@edu.p.lodz.pl>
2017-03-30 14:27:44 +0200

    Create tests for Stacks factory created in point 15.

---
+++ src/test/java/edu/kis/vh/stacks/unittests/StacksUsingListFactoryTest.java
@@ -0,0 +1,40 @@
+package edu.kis.vh.stacks.unittests;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.kis.vh.stacks.Stack;
+import edu.kis.vh.stacks.StackFIFO;
+import edu.kis.vh.stacks.StackHanoi;
+import edu.kis.vh.stacks.factory.DefaultStacksFactory;
+
+public class StacksUsingListFactoryTest {
+	
+	private DefaultStacksFactory factory;
+	
+	@Before
+	public void init() {
+		this.factory = new DefaultStacksFactory();
+	}
+	
+	@Test
+	public void testGetStandardStack() {
+		Assert.assertTrue(factory.getStandardStack() instanceof Stack);
+	}
+	
+	@Test
+	public void testGetFalseStack() {
+		Assert.assertTrue(factory.getFalseStack() instanceof Stack);
+	}
+	
+	@Test
+	public void testGetFIFOStack() {
+		Assert.assertTrue(factory.getFIFOStack() instanceof StackFIFO);
+	}
+	
+	@Test
+	public void testGetHanoiStack() {
+		Assert.assertTrue(factory.getHanoiStack() instanceof StackHanoi);
+	}
+}
