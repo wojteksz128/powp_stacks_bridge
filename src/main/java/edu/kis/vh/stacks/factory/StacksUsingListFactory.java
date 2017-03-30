4af1f0b7ffff14500eb53b7b85ea7c0c08223e76
Szczepaniak.Wojciech <195067@edu.p.lodz.pl>
2017-03-30 11:19:09 +0200

    Create stacks factory, who use list implementation of stack.

---
+++ src/main/java/edu/kis/vh/stacks/factory/StacksUsingListFactory.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package edu.kis.vh.stacks.factory;
+
+import edu.kis.vh.stacks.Stack;
+import edu.kis.vh.stacks.StackFIFO;
+import edu.kis.vh.stacks.StackHanoi;
+import edu.kis.vh.stacks.stackimplementation.StackArray;
+import edu.kis.vh.stacks.stackimplementation.StackList;
+
+/**
+ * Methods in this factory create stacks, who use {@link StackList} implementation of stack.
+ * 
+ * @author Wojciech Szczepaniak
+ *
+ */
+public class StacksUsingListFactory implements StacksFactory {
+
+	/* (non-Javadoc)
+	 * @see edu.kis.vh.stacks.factory.StacksFactory#getStandardStack()
+	 */
+	@Override
+	public Stack getStandardStack() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.kis.vh.stacks.factory.StacksFactory#getFalseStack()
+	 */
+	@Override
+	public Stack getFalseStack() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.kis.vh.stacks.factory.StacksFactory#getFIFOStack()
+	 */
+	@Override
+	public StackFIFO getFIFOStack() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.kis.vh.stacks.factory.StacksFactory#getHanoiStack()
+	 */
+	@Override
+	public StackHanoi getHanoiStack() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
